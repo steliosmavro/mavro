de93f26e5115b124fb712da73c0ae262aefe6b37
Stylianos Mavrokoukoulakis <stylianos.mavrokoukoulakis@gmail.com>
2025-06-21 18:10:59 +0300

    chore: format codebase with updated Prettier config

--- apps/mavrodev/eslint.config.mjs
+++ apps/mavrodev/eslint.config.mjs
@@ -1,7 +1,7 @@
-import { nextJsConfig } from "@repo/eslint-config/next-js";
+import { nextJsConfig } from '@repo/eslint-config/next-js';
 
 /** @type {import("eslint").Linter.Config} */
 export default [
-  ...nextJsConfig,
-  // Other configurations
+    ...nextJsConfig,
+    // Other configurations
 ];
