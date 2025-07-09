18d1fd6b343f07042b9d8902fb936418f4c6f64b
Stylianos Mavrokoukoulakis <stylianos.mavrokoukoulakis@gmail.com>
2025-07-09 10:09:26 +0300

    fix(mavrodev/contact): success message

--- apps/mavrodev/src/app/contact/page.tsx
+++ apps/mavrodev/src/app/contact/page.tsx
@@ -263,7 +263,7 @@ export default function ContactPage() {
 
                                 {/* Success message */}
                                 <AnimatePresence>
-                                    {!submitted && (
+                                    {submitted && (
                                         <motion.div
                                             initial={{ opacity: 0, scale: 0.9 }}
                                             animate={{ opacity: 1, scale: 1 }}
